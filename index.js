9a4c4dd7ee94ffbd145221edc1f1f9db5aea4bce
Ayan Chakraborty <ac4462383@gmail.com>
2023-01-26 11:54:55 +0530

    Added support for capital letters

--- index.js
+++ index.js
@@ -43,6 +43,34 @@ function makeSound(key) {
       let kick = new Audio("sounds/kick-bass.mp3");
       kick.play();
       break;
+    case "W":
+      let tom12 = new Audio("sounds/tom-1.mp3");
+      tom12.play();
+      break;
+    case "A":
+      let tom22 = new Audio("sounds/tom-2.mp3");
+      tom22.play();
+      break;
+    case "S":
+      let tom32 = new Audio("sounds/tom-3.mp3");
+      tom32.play();
+      break;
+    case "D":
+      let tom42 = new Audio("sounds/tom-4.mp3");
+      tom42.play();
+      break;
+    case "J":
+      let snare2 = new Audio("sounds/snare.mp3");
+      snare2.play();
+      break;
+    case "K":
+      let crash2 = new Audio("sounds/crash.mp3");
+      crash2.play();
+      break;
+    case "L":
+      let kick2 = new Audio("sounds/kick-bass.mp3");
+      kick2.play();
+      break;
     default:
       break;
   }
